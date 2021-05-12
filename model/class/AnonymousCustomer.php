d0326a2fd8cc1c64e006f97b047d303d5523e9a9
Neckfly <baptiste.mqt91@gmail.com>
2021-05-12 12:15:57 +0200

    Update anonymous customer class

--- model/class/AnonymousCustomer.php
+++ model/class/AnonymousCustomer.php
@@ -1,7 +1,8 @@
 <?php
 class AnonymousCustomer {
 	
-	function mediaSearch($keyword) {
+	/*---------- Public functions ----------*/
+	public function mediaSearch($keyword) {
 		$db = dbConnect();
 		
 		$req = $db->prepare("SELECT mid, format, title, author, quantity, kind, type FROM media WHERE title LIKE ? OR author LIKE ?");
@@ -10,7 +11,7 @@ class AnonymousCustomer {
 		return $req;	
 	}
 	
-	function mediaPage($mid) {
+	public function mediaPage($mid) {
 		$db = dbConnect();
 		
 		$req = $db->prepare("SELECT mid, format, title, author, price, quantity, kind, description, releaseDate, type, mediaType type FROM media WHERE mid = ?");
@@ -19,7 +20,7 @@ class AnonymousCustomer {
 		return $req;
 	}
 	
-	function roomSearch() {
+	public function roomSearch() {
 		$db = dbConnect();
 		
 		$req = $db->prepare("SELECT number, capacity FROM salle");
@@ -28,7 +29,7 @@ class AnonymousCustomer {
 		return $req;	
 	}
 	
-	function roomPage($number) {
+	public function roomPage($number) {
 		$db = dbConnect();
 		
 		$req = $db->prepare("SELECT * FROM salle WHERE number = ?");
@@ -37,77 +38,148 @@ class AnonymousCustomer {
 		return $req;
 	}
 	
-	function createClientAccount($lastName, $firstName, $email, $gender, $adress, $password, $premium) {
-		$db = dbConnect();
+	public function createClientAccount($lastName, $firstName, $email, $gender, $adress, $password, $cpassword, $premium) {
+		$res = isValidPassword($email, $password, $cpassword);
 		
-		$req = $db->prepare("INSERT INTO client (lastName, firstName, email, gender, adress, password, premium) VALUES(?, ?, ?, ?, ?, ?, ?)");
-		$req->execute(array($lastName, $firstName, $email, $gender, $adress, $password, $premium));
-		
-		return $req;
+		if($res === true) {			
+			$db = dbConnect();
+			
+			$req = $db->prepare("INSERT INTO client (lastName, firstName, email, gender, adress, password, premium) VALUES(?, ?, ?, ?, ?, ?, ?)");
+			$req->execute(array($lastName, $firstName, $email, $gender, $adress, $password, $premium));
+			
+			return $req;
+		}
+		return $res	
 	}
 	
-	function createProviderAccount($compagnyName, $email, $password, $adress) {
-		$db = dbConnect();
+	public function createProviderAccount($compagnyName, $email, $password, $cpassword, $adress) {
+		$res = isValidPassword($email ,$password, $cpassword);
 		
-		$req = $db->prepare("INSERT INTO fournisseur (compagnyName, email, password, adress) VALUES(?, ?, ?, ?)");
-		$req->execute(array($compagnyName, $email, $password, $adress));
-		
-		return $req;
+		if($res === true) {		
+			$db = dbConnect();
+			
+			$req = $db->prepare("INSERT INTO fournisseur (compagnyName, email, password, adress) VALUES(?, ?, ?, ?)");
+			$req->execute(array($compagnyName, $email, $password, $adress));
+			
+			return $req;
+		}
+		return $res	
 	}
 	
-	function Authenticate($email, $password) {
-		$db = dbConnect();
+	public function Authenticate($email, $password) {
+		$res = isValidPassword($email, $password, $password);
 		
-		// Is client
-		$req = $db->prepare('SELECT null FROM client WHERE email = ? and password = ?');	
-		$req->execute(array($email, $password));	
-		
-		if($req->rowCount() > 0) {
-			$_SESSION['status'] = 'customer';
-			return true;
-		}			
-		
-		// Is provider
-		$req = $db->prepare('SELECT null FROM fournisseur WHERE email = ? and password = ?');	
-		$req->execute(array($email, $password));	
-		
-		if($req->rowCount() > 0) {
-			$_SESSION['status'] = 'provider';
-			return true;
-		}	
-		
-		// Is manager
-		$req = $db->prepare('SELECT null FROM gestionnaire WHERE email = ? and password = ?');	
-		$req->execute(array($email, $password));	
-		
-		if($req->rowCount() > 0) {
-			$_SESSION['status'] = 'manager';
-			return true;
-		}	
-		
-		// Is administrator
-		$req = $db->prepare('SELECT null FROM administrateur WHERE email = ? and password = ?');	
-		$req->execute(array($email, $password));	
-		
-		if($req->rowCount() > 0) {
-			$_SESSION['status'] = 'administrator';
-			return true;
-		}	
-		
-		return false;	
+		if($res === true) {			
+			$passwordHash = password_hash($searched_password, PASSWORD_DEFAULT);
+			
+			$db = dbConnect();
+			
+			// Is client
+			$req = $db->prepare('SELECT null FROM client WHERE email = ? and password = ?');	
+			$req->execute(array($email, $password));	
+			
+			if($req->rowCount() > 0) {
+				$_SESSION['status'] = 'customer';
+				return true;
+			}			
+			
+			// Is provider
+			$req = $db->prepare('SELECT null FROM fournisseur WHERE email = ? and password = ?');	
+			$req->execute(array($email, $password));	
+			
+			if($req->rowCount() > 0) {
+				$_SESSION['status'] = 'provider';
+				return true;
+			}	
+			
+			// Is manager
+			$req = $db->prepare('SELECT null FROM gestionnaire WHERE email = ? and password = ?');	
+			$req->execute(array($email, $password));	
+			
+			if($req->rowCount() > 0) {
+				$_SESSION['status'] = 'manager';
+				return true;
+			}	
+			
+			// Is administrator
+			$req = $db->prepare('SELECT null FROM administrateur WHERE email = ? and password = ?');	
+			$req->execute(array($email, $password));	
+			
+			if($req->rowCount() > 0) {
+				$_SESSION['status'] = 'administrator';
+				return true;
+			}
+			
+			return false;
+		} 
+		return $res					
 	}
 	
-	function forgottenPassword($email) {
+	public function forgottenPassword($email) {
 		$db = dbConnect();
 
 		$req = $db->prepare('SELECT null FROM client WHERE email = ?');	
-		$req->execute(array($email, $password));	
+		$req->execute(array($email));	
 		
-		if($req->rowCount() > 0) {
+		if($req->rowCount() > 0) {		
+			forgottenPasswordMail($email);
+		}		
+	}
+	
+	/*---------- Private functions ----------*/
+	private function forgottenPasswordMail($destination) {
+		$mail = new PHPMailer();
+		$mail->isSMTP();
+		$mail->Host = "smtp.gmail.com";
+		$mail->Port = 587;
+		$mail->SMTPAuth = true;
+		$mail->CharSet = 'UTF-8';
+		// Authentification
+		$mail->Username = 'mediatheque.noreply@gmail.com';
+		$mail->Password = 'projetGLA';
+
+		$mail->setFrom('mediatheque.noreply@gmail.com');
+
+		$mail->addAddress($destination);
+
+		$mail->Subject = 'Réinitialisation de votre mot de passe sur votre médiathèque en ligne !';
+		$mail->Body = "<p>Voici votre lien de réinitialisation de votre mot de passe:</p> <strong>lien ici</strong>";
+		$mail->IsHTML(true);
+
+		if($mail->send()) {
 			return true;
+		} else {
+			return false;
+		}    	
+	}
+	
+	private isValidLogin($email, $password, $cpassword) {
+		// Check email
+		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			return 'invalidEmail';
 		}
 		
-		return false;		
+		// Check password difference
+		if($password !== $cpassword) {
+			return 'differentPassword';
+		}
+		
+		// Check password length
+		if(strlen($password) < 8) {
+			return 'shortPassword';
+		}
+		
+		if(strlen($password) > 32) {
+			return 'longPassword';
+		}
+		
+		// Check password valid character
+		$pwdValid = array('-', '_', '!');
+		if(!ctype_alnum(str_replace($pwdValid, '', $password))) {
+			return 'invalidPassword';
+		}
+		
+		return true;
 	}
 	
 }
