9707b65d8a1388e89e268efdd817f7d29fd4de47
Neckfly <baptiste.mqt91@gmail.com>
2021-05-23 21:57:56 +0200

    error database

--- model/db.php
+++ model/db.php
@@ -4,7 +4,7 @@ function dbConnect()
 {
     try
     {
-        $db = new PDO('mysql:host=localhost;dbname=mediatheque;charset=utf8', 'root', 'password123456');
+        $db = new PDO('mysql:host=localhost;dbname=mediatheque;charset=utf8', 'root', 'password');
         return $db;
     }
     catch(Exception $e)
