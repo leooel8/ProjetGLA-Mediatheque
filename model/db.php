22b31bf57eb663d108d664bbb34cd45f6e211f37
jf-pnj <jfgassie@outlook.com>
2021-05-25 11:09:46 +0200

    Update db.php

--- model/db.php
+++ model/db.php
@@ -4,7 +4,7 @@ function dbConnect()
 {
     try
     {
-        $db = new PDO('mysql:host=localhost;dbname=mediatheque;charset=utf8', 'root', 'password');
+        $db = new PDO('mysql:host=localhost;dbname=mediatheque;charset=utf8', 'root', 'password123456');
         return $db;
     }
     catch(Exception $e)
