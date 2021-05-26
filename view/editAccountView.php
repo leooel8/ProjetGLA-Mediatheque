933f55b0f97b55a96e1106626f1e845d2a5e9d01
Neckfly <baptiste.mqt91@gmail.com>
2021-05-26 14:09:30 +0200

    Fix bug in edit account

--- view/editAccountView.php
+++ view/editAccountView.php
@@ -16,10 +16,25 @@
 						
 		<main>
 			<form action='index.php' method='post' id='info'>
-				<p>Nom: <input type='text' name='lastName' maxlength='32' value=<?=htmlspecialchars("'$account[lastName]'")?>> </p>
-				<p>Prénom: <input type='text' name='firstName' maxlength='32' value=<?=htmlspecialchars("'$account[firstName]'")?>> </p>
+				<?php
+				if($_SESSION['status'] === 'provider') {
+					echo "<p>Nom de l'entreprise: <input type='text' name='companyName' maxlength='32' value=" . htmlspecialchars("'$account[companyName]'") . "> </p>";
+				} else {
+					echo "<p>Nom: <input type='text' name='lastName' maxlength='32' value=" . htmlspecialchars("'$account[lastName]'") . "> </p>";
+					echo "<p>Prénom: <input type='text' name='firstName' maxlength='32' value=" . htmlspecialchars("'$account[firstName]'") . "> </p>";
+				}
+				?>
+				
 				<p>Email: <input type='text' name='email' maxlength='46' value=<?=htmlspecialchars($account['email'])?>> </p> 
-				<p>Genre: homme<input type='radio' name='gender' value=0 <?php if($account['gender'] == 0) echo "checked"; ?>> femme<input type='radio' name='gender' value=1 <?php if($account['gender'] == 1) echo "checked"; ?>> </p>			
+				<?php
+				if($_SESSION['status'] === 'customer') {
+					echo "<p>Genre: homme<input type='radio' name='gender' value=0";
+					if($account['gender'] == 0) echo " checked"; 
+					echo"> femme<input type='radio' name='gender' value=1";
+					if($account['gender'] == 1) echo " checked"; 
+					echo "> </p>";	
+				}
+				?>
 				<p>Adresse: <input type='text' name='adress' maxlength='128' value=<?=htmlspecialchars("'$account[adress]'")?>> </p>
 			
 				<input type='submit' id='validEdition' name='validEdition' value='Valider les modifications'>
